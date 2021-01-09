401324a8b345f49db117d402103854c09d6c0d8d
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-01-09 05:50:34 -0800

    Completes most server functionality

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+
+const typeDefs = gql`
+type Query {
+  me: User
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(favorite: BookInput): User
+  removeBook(bookId: ID!): User
+}
+
+input BookInput {
+  bookId: ID!
+  authors: [String]
+  description: String!
+  title: String!
+  image: String
+  link: String
+}
+
+type User {
+  _id: ID
+  username: String!
+  email: String!
+  bookCount: Int
+  savedBooks: [Book]
+}
+
+type Book {
+  bookId: ID!
+  authors: [String]
+  description: String!
+  title: String!
+  image: String
+  link: String
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+`;
+
+module.exports = typeDefs;
